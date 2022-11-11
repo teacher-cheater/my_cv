feeb4b44a5967d368bc85e4cb7e765d1c9fbac26
teacher_cheater <johnnykayfun@gmail.com>
2022-11-11 18:39:54 +0300

    fix bugs

--- src/components/Information/InformationAboutMe.jsx
+++ src/components/Information/InformationAboutMe.jsx
@@ -7,18 +7,19 @@ function InformationAboutMe() {
       <div className={s.block}>
         <p className={s.text}>Hi all. I am</p>
         <h1 className={s.name}>Nekipelov Evgeniy</h1>
-        <a className={s.profession} href="#">
-          Front-end developer
-        </a>
+        <p className={s.profession}> > Front-end developer</p>
       </div>
       <div className={s.block}>
         <p className={s.info}>// complete the game to continue</p>
         <p className={s.info}>// you can also see it on my Github page</p>
-        <a className={s.link} href="#">
-          <span className={s.const}>const</span> const{" "}
+        <div className={s.link}>
+          <span className={s.const}>const</span>{" "}
           <span className={s.github}>githubLink</span> =
-          <span className={s.project}>“https://github.com/example/url”</span>
-        </a>
+          <span className={s.project}>
+            {" "}
+            “https://github.com/teacher-cheater”
+          </span>
+        </div>
       </div>
     </>
   );
